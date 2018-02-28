f2cb58c97cd32b6119c44f0717485dd164259e60
zhogova <Z3752kpm>
2018-02-28 16:16:20 +0200

    Subscruble test

--- src/test/java/com/rulla/FirstTest.java
+++ src/test/java/com/rulla/FirstTest.java
@@ -35,10 +35,30 @@ public class FirstTest extends WebDriverSetings{
         String mailUser =  ProfilUser.getText();
         Assert.assertEquals("E-Mail: zhogov@trud.com", mailUser);
 
-
     }
 
+    @Test
+    public void subscrible() {
+        driver.get("https://ca.rulla.com/account/login.html");
+        WebElement Loginfield = driver.findElement(By.id("LoginForm_email"));
+        Loginfield.sendKeys("zhogov@trud.com");
+        WebElement Password = driver.findElement(By.id("LoginForm_password"));
+        Password.sendKeys("awdasd");
+        WebElement LoginButton = driver.findElement(By.xpath("//button[1]"));
+        LoginButton.click();
+        WebElement ButtonSettings = driver.findElement(By.xpath("//a[3]"));
+        ButtonSettings.click();
+        driver.get("https://ca.rulla.com/office/createsubscription.html");
+        WebElement cityField   = driver.findElement(By.id("CrmCreateSubForm_region_0"));
+        cityField.sendKeys("Toronto");
+        WebElement vocationField = driver.findElement(By.id("CrmCreateSubForm_query_0"));
+        vocationField.sendKeys("Acquisition Logistics Analyst");
+        WebElement subscribeAcc = driver.findElement(By.xpath("//button[@type='submit']"));
+        subscribeAcc.click();
 
+
+    }
+    
 }
 
 
