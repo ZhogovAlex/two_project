0e959b9e3451c75a296e9854e00d0aa65f56899b
zhogova <Z3752kpm>
2018-02-28 17:24:55 +0200

    Subscruble test

--- src/test/java/com/rulla/FirstTest.java
+++ src/test/java/com/rulla/FirstTest.java
@@ -55,10 +55,31 @@ public class FirstTest extends WebDriverSetings{
         vocationField.sendKeys("Acquisition Logistics Analyst");
         WebElement subscribeAcc = driver.findElement(By.xpath("//button[@type='submit']"));
         subscribeAcc.click();
+    }
+
+    @Test
+
+    public void subrscrible1click() {
+        driver.get("https://ca.rulla.com/account/login.html");
+        WebElement Loginfield = driver.findElement(By.id("LoginForm_email"));
+        Loginfield.sendKeys("zhogov@trud.com");
+        WebElement Password = driver.findElement(By.id("LoginForm_password"));
+        Password.sendKeys("awdasd");
+        WebElement LoginButton = driver.findElement(By.xpath("//button[1]"));
+        LoginButton.click();
+        WebElement searchBtn = driver.findElement(By.xpath("//button[@class='search-btn']"));
+        searchBtn.click();
+        WebElement subscribleBtn = driver.findElement(By.xpath("//button[@class='btn btn-blue-shd rippled']"));
+        subscribleBtn.click();
 
 
     }
-    
+
+
+
+
+
+
 }
 
 
